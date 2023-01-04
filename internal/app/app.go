571aae67fa0a593651a652f9590b31cebee44f68
Roman Abramenko <abramenkoroman22@gmail.com>
2023-01-04 06:55:23 +0300

    first commit

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,23 @@
 package app
 
 import (
+	"fmt"
 	server "github.com/avalonprod/gasstrem--back-end/internal/server"
 	"log"
+	"net/http"
 )
 
 const PORT string = "8000"
 
 func HandlerApp() {
 	server := new(server.Server)
+	http.HandleFunc("/", homePage)
 	if err := server.Run(PORT); err != nil {
 		log.Fatalf("error accured whiule running http server: %v", err)
 	}
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server has been started...")
 }
